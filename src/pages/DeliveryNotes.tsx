411a3f02640ab28f1be30b9b18d3b4287dd14a12
tsilicanidev <tsilicani@gmail.com>
2025-05-13 13:30:29 -0300

    Update DeliveryNotes.tsx to reduce spacing for two copies on one A4 page

--- src/pages/DeliveryNotes.tsx
+++ src/pages/DeliveryNotes.tsx
@@ -326,7 +326,7 @@ body {
   flex-direction: column;
   gap: 0.2cm;
   justify-content: space-between;
-  height: 28cm; /* Deixa espaço para impressoras */
+  height: 20cm; /* Deixa espaço para impressoras */
 }
 
 .header h1 {
